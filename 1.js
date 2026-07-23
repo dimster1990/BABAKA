67a9d8daa336dd85e33e6af351a803cd7ec4863e
dimster1990 <dimster1990@gmail.com>
2026-07-23 16:22:31 +0300

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,37 +1,55 @@
-let _main_menu = 
-                '<div class = "menu_link" id = "menu_link_1" onclick = "main_menu_items(0)" alt = "About project">About project</div>' +
-                '<div class = "menu_link" id = "menu_link_2" onclick="main_menu_items(1)">Книга</div>' +
-                '<div class = "menu_link" id = "menu_link_3" onclick="main_menu_items(2)">menu link 3</div>' +
-                '<div class = "menu_link" id = "menu_link_4" onclick="main_menu_items(3)">menu link 4</div>' +
-                '<div class = "menu_link" id = "menu_link_5" onclick="main_menu_items(4)">menu link 5</div>' +
-                '<div class = "menu_link" id = "menu_link_6" onclick="main_menu_items(5)">menu link 6</div>';
+let _main_menu = ['About project', 'The book', 'Galary', '4', '5', '6'];
 
 
 let _cash_item_number = localStorage.getItem("cash_item_number") != null ? localStorage.getItem("cash_item_number") : 0;
 let _book_cash_page_number = localStorage.getItem("book_cash_page_number") != null ? localStorage.getItem("book_cash_page_number") : 0;
+let _book_cash_page_scrolltop = localStorage.getItem("book_cash_page_scrolltop") != null ? localStorage.getItem("book_cash_page_scrolltop") : 0;
 
-
-
+for (let i = 0; i < _main_menu.length; i++)
+    {
+        main_menu.innerHTML += '<div class = "menu_link" id = "menu_link_' + i +'" onclick = "main_menu_items(' + i +')" title = "' + _main_menu[i] + '">' + _main_menu[i] + '</div>';
+    }
 
 function main_menu_items(_item)
 {
-    main_menu.innerHTML = _main_menu;
-    item_marker(_item);
-
+    //main_menu.innerHTML = _main_menu;
     
+    //item_marker(_item);
+
     localStorage.setItem("cash_item_number", _item);
 
     if(_item == 0)
     {
-        tester(1);
+        about();
+        //alert(menu_link_1.className == "menu_link_active");
     }
 
     if(_item == 1)
     {
-        book();
+        menu_link_1.className == "menu_link" ? book() + book_scroll_load() : null;
     }
 
+    if(_item == 0)
+    {
+       // tester(1);
+    }
+
+    if(_item == 0)
+    {
+        //tester(1);
+    }
+
+    if(_item == 0)
+    {
+        //tester(1);
+    }
     
+    if(_item == 0)
+    {
+        //tester(1);
+    }
+
+    item_marker(_item);
 }
 
 function item_marker(_item)
@@ -57,3 +75,16 @@ function view_off()
 
 
 main_menu_items(_cash_item_number);
+
+
+setInterval(() => 
+    {
+        if(menu_link_1.className == "menu_link_active")
+        {
+            if(tester.textContent != book_pages.scrollTop)
+            {
+                localStorage.setItem("book_cash_page_scrolltop", book_pages.scrollTop);
+                tester.innerHTML = book_pages.scrollTop;
+            }
+        }
+    }, 1000);
