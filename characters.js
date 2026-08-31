3100cd836cbf3f32f98e6de5bcce37973b23e2ef
dimster1990 <dimster1990@gmail.com>
2026-08-31 16:35:43 +0300

    Add files via upload

--- characters.js
+++ characters.js
@@ -1,4 +1,30 @@
+
+
 function characters()
 {
-    content.innerHTML = "------------- Алиллуйя --------------------";
+    content.innerHTML = "<div class = 'characters_content'>" +
+                            "<div class = 'characters_scroll'>" +
+                                "<div id = 'characters'>" +
+                                    "<div class = 'character'></div>"+
+                                    "<div class = 'character'></div>"+
+                                    "<div class = 'character'></div>"+
+                                    "<div class = 'character'></div>"+
+                                    "<div class = 'character'></div>"+
+                                    "<div class = 'character'></div>"+
+                                    "<div class = 'character'></div>"+
+                                    "<div class = 'character'></div>"+
+                                    "<div class = 'character'></div>"+
+                                "</div>"+
+                            "</div>" +
+                            "<div id = 'characters_image'></div>" +
+                            "<div id = 'characters_text'></div>" +
+                        "</div>";
+}
+
+function characters_list()
+{
+    for (let i = 0; i < _book_page_name.length; i++)
+    {
+        book_content_links.innerHTML += '<div class = "menu_link_page" id = "page_list_' + i + '"' + ' onclick = "book_menu(' + i + ')">' + _book_page_name[i] + '</div>'
+    }
 }
